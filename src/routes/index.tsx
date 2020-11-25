92ae28aa038896c7c49e38af27169d2229b973fc
Mariana <marianadealmeidapereira@outlook.com>
2020-11-25 20:04:37 -0300

    add private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import Login from '../pages/Login';
 import Main from '../pages/Main';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" component={Login} />
-    <Route path="/home" exact component={Main} />
+    <Route path="/" exact component={Login} />
+    <Route path="/home" component={Main} isPrivate />
   </Switch>
 );
 
